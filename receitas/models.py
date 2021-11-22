abdbb9cebe16010d5a0ad962b6a7bfc1c4fa2e5c
Decripter <jeilsonaraujo@icloud.com>
2021-11-22 16:49:43 -0300

    conf: criado o modelo que armazena as receitas e os chefs

--- receitas/models.py
+++ receitas/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+class Chef(models.Model):
+    nome_chef = models.CharField('Nome', max_length=200, unique=True)
+
+    def __str__(self):
+        return self.nome_chef
+
+
+
+class Receita(models.Model):
+    chef = models.ForeignKey(Chef, on_delete=models.CASCADE)
+    receita = models.TextField('Receita')
+
+    def __str__(self):
+        return self.choice_text
