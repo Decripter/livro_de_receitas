624128aa947de269876e3d9ccf3126d61e9f8de2
Decripter <jeilsonaraujo@icloud.com>
2021-11-23 11:32:23 -0300

    feat: adicionado a funcao de filtrar por chef e a funcao de busca por receitas

--- client/views.py
+++ client/views.py
@@ -8,5 +8,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 class ReceitasViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = ReceitaModel.objects.all()
     serializer_class = ReceitaSerializer 
-    filter_backends = [filters.SearchFilter]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['chef']
+
     search_fields = ['receita']
