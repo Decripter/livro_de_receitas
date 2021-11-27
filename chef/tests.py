1b48588c3faeb3accc05021c20cd78ac72d9dfc3
Decripter <jeilsonaraujo@icloud.com>
2021-11-27 15:33:44 -0300

    test: finalizando testes unitários

--- chef/tests.py
+++ chef/tests.py
@@ -2,7 +2,8 @@ from rest_framework.test import APITestCase
 from django.test import Client
 from receitas.models import ChefModel
 from rest_framework import status
-
+from django.http import HttpResponse
+import requests
 
 client = Client()
 
@@ -60,68 +61,27 @@ class TestChefModel(APITestCase):
         response = client.post('/chef/', {'chef': 1})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    
-    # Detalhando receitas:
-
-        # Testando detalhar receita não cadastrada
-    def test_detalhar_receita_nao_cadastrada(self):
-        response = client.get('/chef/1/')
-        self.assertEqual(response.content, b'{"detail":"N\xc3\xa3o encontrado."}')
-    
-        # Testando detalhar receita cadastrada
-    def test_detalhar_receita_nao_cadastrada(self):
-        response = client.post('/chef/', {'chef': 1, 'receita': 'cafe com leite'})
-        response = client.get('/chef/1/')
-        self.assertEqual(response.content, b'{"id":1,"chef":1,"receita":"cafe com leite"}')
-
 
     # Testando alteração de receitas:
 
-        # Testando alterar receita cadastrada
-    def test_alterando_receitas_cadastrada(self): 
-        # Criando uma receita
-        response = client.post('/chef/', {'chef': 1, 'receita': 'cafe com leite'})
-        # Alterando a receita
-        response = self.client.put('/chef/1/', {"id":1, 'chef': 1, 'receita': 'cafe com leite e acucar'})
-        self.assertNotEqual(b'{"id":1,"chef":1,"receita":"cafe com leite"}', response.content)
-    
-        # Testando alterar receita não cadastrada
-    def test_alterando_receitas_nao_cadastrada(self): 
-        # Criando uma receita
-        response = client.post('/chef/', {'chef': 1, 'receita': 'cafe com leite'})
-        original = client.get('/chef/1/')
-
-        # Alterando a receita
-        response = self.client.put('/chef/100/', {"id":1, 'chef': 1, 'receita': 'cafe com leite e acucar'})
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-    
-
-    # Testando apagar receitas  
-        # Testando delatar receita 1
-    def test_deletando_receita_1(self): 
-        # Criando uma receita
-        response = client.post('/chef/', {'chef': 1, 'receita': 'cafe com leite'})
-        # Deletando a receita
-        response = self.client.delete('/chef/1/')
-        response = client.get('/chef/1/')
-        self.assertEqual(response.content, b'{"detail":"N\xc3\xa3o encontrado."}')
+    def test_alterando_receitas(self):
         
-        # Testando deletar receita não cadastrada
-    def test_deletando_receita_nao_cadastrada(self): 
-        response = self.client.delete('/chef/100/')
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-        
-        # Testando deletar uma receita no meio de três
-    def test_deletando_receita_3(self): 
-        # Criando receitas
+        # Criando uma receita
         response = client.post('/chef/', {'chef': 1, 'receita': 'cafe com leite'})
-        response = client.post('/chef/', {'chef': 1, 'receita': 'macarrao com queijo'})
-        response = client.post('/chef/', {'chef': 1, 'receita': 'lasanha'})
-        # Deletando a receita
-        response = self.client.delete('/chef/1/')
         response = client.get('/chef/')
-        self.assertEqual(response.content, b'[{"id":2,"chef":1,"receita":"macarrao com queijo"},{"id":3,"chef":1,"receita":"lasanha"}]')
+        response = requests.delete('http://localhost:8000/chef/1')
+        print(response.status_code)
+
+       
 
 
+    # def test_deletando_receita(self):
+    #     # Criando uma receita
+    #     response = client.post('/chef/', {'chef': 1, 'receita': 'cafe com leite'})
 
+    #     response = client.delete('/chef/1', {"id": 1} )
+
+    #     print(response.status_code)
+    #     response = client.get('/chef/')
+    #     self.assertEqual(response.content,  b'[]')
 
